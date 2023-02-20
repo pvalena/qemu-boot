f6aeeabdbdf0ee5a254fdbc47b35857b125903c2
Pavel Valena <pvalena@redhat.com>
2023-02-20 23:57:41 +0100

    Enhance boot.sh

--- boot.sh
+++ boot.sh
@@ -27,14 +27,15 @@
 #   ./boot.sh -u 'rhel9-22-03-18'
 #
 
+## Init
+
 bash -n "$0" || exit 1
 set -xe
 
 cd "$(dirname "$0")"
 
-t () {
-  [[ "$1" == "-t" ]] && shift && exec timeout 60 "$0" "$@"
-}
+
+## Methods
 
 abort () {
   echo "$@" >&2
@@ -64,27 +65,73 @@ ovmf () {
   $SUDO $(which dnf) install '/usr/share/edk2/ovmf/OVMF_CODE.fd'
 }
 
+escape () {
+  printf "%q" "$1"
+
+  return 0
+}
+
+## Const
+
+DEFAULT_NET='-device virtio-net-pci,netdev=net0 -netdev user,id=net0,ipv6=off'
+
+
+## Args
+
+# Pre-switches
+[[ "$1" == "-d" ]] && {
+  DEV="-device $2"
+  shift 2
+  :
+} || DEV=
+
+[[ "$1" == "-n" ]] && {
+  NET="$2"
+  shift 2
+
+  [[ -n "$NET" && "$NET" != '-' ]] \
+    && NET="${NET:+-nic $NET}" \
+    || NET=
+  :
+} || NET="$DEFAULT_NET"
+
+[[ "$1" == "-t" ]] && {
+  shift
+  exec timeout 60 "$0" "$@"
+}
+
+
+# Main arg
 ARG="$1"
 shift
-[[ -z "$ARG" ]] && abort "Arg missing!"
+[[ -z "$ARG" || "${ARG:0:1}" == "-" ]] \
+  && abort "Arg missing or invalid!"
 
+# Disk image string
 [[ -n "$1" ]] && {
   DSK="$1"
+  shift
 }
 DSK="disk${DSK:+-$DSK}.qcow2"
 [[ -r "$DSK" ]] || abort "Disk missing or unreadable: $DSK"
 
+# Shift even if empty
 shift ||:
 
-[[ -n "$1" ]] && {
-  IMG="$1"
-  shift
-}
-IMG="$(iso ${IMG:-\-dvd\-})"
+# Iso file for the cdrom
+[[ "${1:0:1}" != "-" ]] && {
+  [[ -n "$1" ]] && {
+    IMG="$1"
+    shift
+  }
+  IMG="$(iso ${IMG:-})"
+  IMG="${IMG:+-cdrom $IMG}"
+  :
+} || IMG=''
 
-t "$@" ||:
+## Execute
 
-[[ "$ARG" == '-a' ]] && {
+[[ "$ARG" == 'AARCH' ]] && {
   echo "> aarch"
   warn "does not work yet"
 
@@ -98,7 +145,7 @@ t "$@" ||:
     -accel hvf -accel tcg -cpu cortex-a57 -M virt,highmem=off \
     -drive file=/opt/homebrew/share/qemu/edk2-aarch64-code.fd,if=pflash,format=raw,readonly=on \
     -drive if=virtio,file="$DSK" \
-    -cdrom $(iso aarch64-boot) \
+    ${IMG} ${NET} ${DEV} \
     -nographic \
     -boot d
 
@@ -113,13 +160,13 @@ t "$@" ||:
     -device hda-output \
     -device qemu-xhci \
     -device usb-kbd \
-    -device virtio-net-pci,netdev=net \
+    -device virtio-net-pci,netdev=net0 \
     -device virtio-mouse-pci \
-    -netdev user,id=net,ipv6=off \
+    -netdev user,id=net0,ipv6=off \
     -drive "if=pflash,format=raw,file=./edk2-aarch64-code.fd,readonly=on" \
     -drive "if=pflash,format=raw,file=./edk2-arm-vars.fd,discard=on" \
     -drive "if=virtio,format=raw,file=./disk.raw,discard=on" \
-    -cdrom $(iso aarch64-dvd) \
+    ${IMG} ${NET} ${DEV} \
     -nographic \
     -boot d
 
@@ -131,7 +178,7 @@ t "$@" ||:
 
 }
 
-[[ "$ARG" == "-x" ]] && {
+[[ "$ARG" == "EXPERIMENTAL" ]] && {
   echo "> x86 : non-uefi"
   warn "does not work yet"
 
@@ -143,18 +190,18 @@ t "$@" ||:
     -device hda-output \
     -device qemu-xhci \
     -device usb-kbd \
-    -device virtio-net-pci,netdev=net \
+    -device virtio-net-pci,netdev=net0 \
     -device virtio-mouse-pci \
-    -netdev user,id=net,ipv6=off \
+    -netdev user,id=net0,ipv6=off \
     -drive "if=virtio,format=qcow2,file=${DSK},discard=on" \
-    -cdrom "${IMG}" \
+    ${IMG} ${NET} ${DEV} \
     -nographic \
     -boot d
 
   exit
 }
 
-[[ "$ARG" == "-f" ]] && {
+[[ "$ARG" == "DEFAULT" ]] && {
   echo "> x86 : non-uefi"
 
   exec \
@@ -164,14 +211,14 @@ t "$@" ||:
     -accel kvm \
     -smp 1 \
     -drive "file=${DSK},format=qcow2" \
-    -cdrom "$IMG" \
+    ${IMG} ${NET} ${DEV} \
     -m 2G \
     -nographic
 
   exit
 }
 
-[[ "$ARG" == "-u" ]] && {
+[[ "$ARG" == "UEFI" ]] && {
   echo "> x86 : uefi"
 
   ovmf
@@ -184,15 +231,46 @@ t "$@" ||:
     -smp 4 \
     -drive "file=${DSK},format=qcow2" \
     -bios /usr/share/edk2/ovmf/OVMF_CODE.fd \
-    -cdrom "${IMG}" \
+    ${IMG} ${NET} ${DEV} \
     -accel kvm \
     -serial mon:stdio \
-    -device virtio-net-pci,netdev=net \
-    -netdev user,id=net,ipv6=off \
     -nographic \
     -chardev vc,id=vc1,width=$(tput cols),height=$(tput lines) -mon chardev=vc1
 
   exit
+
+    -nic tap,br=virbr0,model=e1000,"helper=/usr/libexec/qemu-bridge-helper" \
+
+    -device virtio-net-pci,netdev=net0 \
+    -netdev user,id=net0,ipv6=off \
+
+    -append 'console=ttyS0' \
+
+  # kernel: net.ifnames.prefix=net inst.text console=ttyS0
+
+}
+
+[[ "$ARG" == "NET" ]] && {
+  echo "> x86 : $ARG"
+
+  exec \
+  qemu-system-x86_64 \
+    -boot n \
+    -m 2G \
+    -cpu max \
+    -smp 1 \
+    ${IMG} ${NET} ${DEV} \
+    -accel kvm \
+    -serial mon:stdio \
+    -nographic
+    -chardev vc,id=vc1,width=$(tput cols),height=$(tput lines) -mon chardev=vc1
+
+  exit
+
+    -nic tap,br=virbr0,mac=52:54:00:12:35:58,model=e1000,"helper=/usr/libexec/qemu-bridge-helper" \
+
+    -device e1000,netdev=net0,mac=52:54:00:12:35:58 \
+    -netdev user,id=net0,ipv6=off \
 
     -append 'console=ttyS0' \
 
